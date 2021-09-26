5da17951814f1687d6d2742ddbdccbed0654bffa
Saikat Saha <robertsaikat011@gmail.com>
2021-09-26 10:29:53 +0600

    Calculation Done

--- src/components/Developer/Developer.js
+++ src/components/Developer/Developer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee, faLink, faUser } from "@fortawesome/free-solid-svg-icons";
 import "./Developer.css";
 
 const Developer = (props) => {
@@ -6,32 +8,54 @@ const Developer = (props) => {
     props.developer;
   return (
     <div className="col-4 ">
-      <div className="card mb-3 set-width">
+      <div className="card mb-3 set-width card-design">
         <div className="d-flex ">
           <div className="col-md-4">
-            <img src={image} className="img-fluid rounded-start" alt="..." />
+            <img
+              src={image}
+              className="img-fluid rounded-start h-100"
+              alt="..."
+            />
           </div>
-          <div className="col-md-8 table card-body">
-            <tr>
-              <th className="card-title">Name: </th>
-              <td>{name}</td>
-            </tr>
-            <tr>
-              <th className="card-title">Age: </th>
-              <td>{age}</td>
-            </tr>
-            <tr>
-              <th className="card-title">Gender: </th>
-              <td>{gender}</td>
-            </tr>
-            <tr>
-              <th className="card-title">Designation: </th>
-              <td>{designation}</td>
-            </tr>
-            <tr>
-              <th className="card-title">Salary: </th>
-              <td>{salary}</td>
-            </tr>
+          <div className="col-md-8 card-body">
+            <table>
+              <tbody>
+                <tr>
+                  <th className="card-title">Name: </th>
+                  <td>{name}</td>
+                </tr>
+                <tr>
+                  <th className="card-title">Age: </th>
+                  <td>{age}</td>
+                </tr>
+                <tr>
+                  <th className="card-title">Gender: </th>
+                  <td>{gender}</td>
+                </tr>
+                <tr>
+                  <th className="card-title">Designation: </th>
+                  <td>{designation}</td>
+                </tr>
+                <tr>
+                  <th className="card-title">Salary: </th>
+                  <td>${salary} per year</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="mt-3 btn-area">
+              <button
+                onClick={() => props.handleAddToTeam(props.developer)}
+                className="btn btn-addtoteam "
+              >
+                <FontAwesomeIcon icon={faUser} /> Add to Team
+              </button>
+              <a href={website}>
+                <button className="btn btn-portfolio ms-1">
+                  <FontAwesomeIcon icon={faLink} /> Portfolio
+                </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,17 +4,30 @@ import "./Main.css";
 
 const Main = () => {
   const [developers, setDevelopers] = useState([]);
+  const [addDevelopers, setAddDevelopers] = useState([]);
   useEffect(() => {
     fetch("./developers.JSON")
       .then((res) => res.json())
       .then((data) => setDevelopers(data));
   }, []);
+
+  const handleAddToTeam = (newDeveloper) => {
+    const addNewDeveloper = [...addDevelopers, newDeveloper];
+    setAddDevelopers(addNewDeveloper);
+  };
+
+  const costReducer = (preCost, currCost) => preCost + currCost.salary;
+
+  const totalCost = addDevelopers.reduce(costReducer, 0);
   return (
     <div>
-      <h2 className="text-center">Build Your Desire Team</h2>
+      <h2 className="text-center text-white">Build Your Desire Team</h2>
+      <h4 className="text-center text-white">Budget: 1 million (yearly)</h4>
       <div className="d-flex justify-content-center mt-4">
-        <h4 className="me-5 bg-total-member">Total Members: 0</h4>
-        <h4 className="bg-total-cost">Total Cost: $0</h4>
+        <h4 className="me-5 bg-total-member">
+          Total Members: {addDevelopers.length}
+        </h4>
+        <h4 className="bg-total-cost">Total Cost: ${totalCost}</h4>
       </div>
 
       <div className="mt-5 ms-5 ">
@@ -23,7 +36,11 @@ const Main = () => {
             <div className="card-container border-end">
               <div className="row g-3 me-3">
                 {developers.map((developer) => (
-                  <Developer developer={developer}></Developer>
+                  <Developer
+                    key={developer.id}
+                    handleAddToTeam={handleAddToTeam}
+                    developer={developer}
+                  ></Developer>
                 ))}
               </div>
             </div>
